112570bf4e8e22d7afd477d2bb677adf72ecec22
ShreyaAcharya004 <159054113+ShreyaAcharya004@users.noreply.github.com>
2024-03-05 15:11:47 +0530

    Update program 18.py

--- program 18.py
+++ program 18.py
@@ -1,4 +1,8 @@
-#write 
+# Write a Python program to add 'ing' at the end of a given string (length 
+should be at least 3). If the given string already ends with 'ing' then add 
+'ly' instead if the string length of the given string is less than 3, leave it 
+unchanged
+
 s=input("enter a string: ")
 if len (s1)>=3:
     if s1.endswitch("ing"):
